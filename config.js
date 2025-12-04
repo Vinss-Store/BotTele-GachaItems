eeb5d0284a8d16c27c7da68000f2b4dd1313b8cc
Yt : Vinss Students <vinssstore08@gmail.com>
2025-12-05 00:28:07 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,21 @@
+// Bot Configuration
+global.TokenBot = '8262811817:AAHU_Cy6OxEEarialm4ODk3ueEaHUDLRxVM'
+global.IdOwner = '7529831376'
+global.OwnerName = 'Vinss Boyz'
+global.BotName = 'Vinss Botz'
+
+// Admin Configuration (bisa lebih dari 1)
+global.AdminList = [
+    '7529831376',  // Owner (Admin 1)
+    '7740390584'    // Admin 2 - Ganti dengan ID admin kedua
+]
+
+// Channel Configuration
+global.IdChanel1 = '-1002881812369'
+global.IdChanel2 = '-1002926478896'
+
+// Invite Link (untuk tombol join channel)
+// Channel private: gunakan invite link
+// Channel public: gunakan @username
+global.InviteLink1 = 'https://t.me/+np9g-pHg3UUwMGVl'
+global.InviteLink2 = '@infobotzvinss'
